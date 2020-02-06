7a7cb5d4b355db8f6743d5338b77225e13e92974
nicolasdelafuente <ni.delafuente@gmail.com>
2020-02-06 20:52:45 -0300

    Parte 1

---
+++ definiciones.py
@@ -0,0 +1,7 @@
+import threading
+import time
+import logging
+
+
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
--- muchosThreads.py
+++ muchosThreads.py
@@ -2,15 +2,32 @@ import threading
 import time
 import logging
 
+from tiempo import Contador
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 # la función para usar para el thread
 def dormir():
-    time.sleep()
+    time.sleep(1)
 
 
+contador = Contador()
+
+contador.iniciar()
+
+lista = [] #creo lista
 
 for i in range(10):
     #crear un thead
+    t = threading.Thread(target=dormir, name='thread')
+    #logging.info('Thread ')
     #lanzarlo
+    t.start()
+    lista.append(t) #los cargo en la lista
+    #t.join()
+
+for thread in lista:
+    thread.join()
+
+contador.finalizar()
+contador.imprimir()
